bf2b0f0b6e21976d9ee3e1e98bbc0f6407c29695
pppest <ppest@protonmail.com>
2023-07-12 21:54:15 -0600

    fix build

--- src/components/ContactMe/ContactMe.tsx
+++ src/components/ContactMe/ContactMe.tsx
@@ -1,6 +1,4 @@
 import Telegram from "./Telegram";
-import Email from "./Email";
-import Discord from "./Discord";
 
 function ContactMe() {
   return (
@@ -8,8 +6,6 @@ function ContactMe() {
       <h1 className="text-6xl font-display normal-case whitespace-nowrap">Contact me!</h1>
       <div className="flex flex-col lg:flex-row space-x-8 items-center justify-center">
         <Telegram />
-        {/* <Discord /> */}
-        <Email />
       </div>
     </section>
   );
