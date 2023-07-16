f9b0bdf819644e2d4a5c9dff8a872d24540ead3e
pppest <ppest@protonmail.com>
2023-07-15 20:31:31 -0600

    set about text justify start

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div id="about" className="flex flex-col gap-8 justify-center items-center w-[80vw] container">
       <h1 className="text-6xl font-display normal-case whitespace-nowrap">About</h1>
-      <div className="flex flex-wrap gap-4 justify-center items-center font-primaryFont text-3xl xl:text-4xl normal-case">
+      <div className="flex flex-wrap gap-4 justify-start items-center font-primaryFont text-3xl xl:text-4xl normal-case">
         <p >
           I have 5 years of experience in the blockchain industry and decentralized applications. I have worked in Web3 on DeFi and NFT
           projects as frontend, smart-contract and lead developer. <br />I am proficient in a range of web3 tools, smart contract
